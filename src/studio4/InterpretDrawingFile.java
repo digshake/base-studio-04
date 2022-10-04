49d8e991e33485e6c732e6597296f05234c51e33
dssho <dssho@DESKTOP-2ASM3QT.dhcp.wustl.edu>
2022-10-04 13:28:45 -0500

    Updating studio 4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,29 +1,24 @@
 package studio4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
 import edu.princeton.cs.introcs.StdDraw;
-import support.cse131.ArgsProcessor;
 
 /**
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class InterpretDrawingFile {
-	public static void readFileViaArgsProcessorAndDraw(ArgsProcessor ap) {
-		
-	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		SwingUtilities.invokeLater(()->{
-			ArgsProcessor ap = DrawingFiles.createArgsProcessorFromFile(args);
-			while (ap != null) {
-				StdDraw.clear();
-				readFileViaArgsProcessorAndDraw(ap);
-				ap = DrawingFiles.createArgsProcessorFromFile(new String[] {});
-			}
-			System.exit(0);
-		});
+		JFileChooser chooser = new JFileChooser("resources");
+		chooser.showOpenDialog(null);
+		File f = new File(chooser.getSelectedFile().getPath());
+		Scanner in = new Scanner(f); //making Scanner with a File
+		
 	}
 }
